4ff3aad3ec68e7a10da0361d9ab75810019f5f82
Jonathan <jfayerz@icloud.com>
2018-10-26 00:12:18 -0400

    Works now. Need to add download of img, conver to mp3, write of metadata

--- Main.py
+++ Main.py
@@ -9,16 +9,16 @@ from datetime import datetime
 from bs4 import BeautifulSoup
 import urllib
 import re
-from mutagen.id3 import ID3,TDAT,TPOS1,TPOS2,TRCK,TIT2
+#from mutagen.id3 import ID3,TDAT,TPOS1,TPOS2,TRCK,TIT2
 
 site_url = 'https://centralteachings.com/4th-street/'
 csv_file = 'results.csv'
 
-speaker = ''
-passage = ''
-title = ''
-CT_date = ''
-speaker_image = ''
+#speaker = ''
+#passage = ''
+#title = ''
+#CT_date = ''
+#speaker_image = ''
 
 audio_files = []
 images = []
@@ -27,10 +27,40 @@ passages = []
 speakers = []
 
 page = urllib.request.urlopen(site_url)
+#print(page)
 soup = BeautifulSoup(page,'html.parser')
+#print(soup)
 items = []
 for x in soup.find_all('div',attrs={'class':'views-row'}):
-    items.append(x)
+    #print(x)
+    for y in x.find_all('div',attrs={'class':'views-field-field-bio-picture'}):
+        print(y)
+        for z in str(y.div.img).split(" "):
+            print(z)
+            if z.find('.JPG') != -1:
+                images.append(re.sub('"','',z.partition("=")[2]))
+                print(images)
+            if z.find('alt') != -1:
+                speakers.append(re.sub('"','',z.partition("=")[2]))
+                print(speakers) #needs to be gotten from elsewhere in case the 
+                                #speaker doesn't have a photo to grab this from
+    for title_tag in x.find_all('div',attrs={'class':'views-field-title'}):
+        titles.append(str(title_tag.span.a.contents))
+        print(titles)
+    for passage_tag in x.find_all('div',attrs={'class':'views-field-field-passage'}):
+        passages.append(str(passage_tag.div.contents))
+        print(passages)
+    for audio_tag in x.find_all('div',attrs={'class':'views-field-field-audio-link'}):
+        for auditem in str(audio_tag.div.a).split(" "):
+            print(auditem)
+            if auditem.find('.m4a') != -1:
+                print(auditem)
+                audio_files.append(re.sub('"','',str(auditem)).partition("=")[2])
+                print(audio_files)
+
+for url in audio_files:
+    print(url)
+    urllib.request.urlretrieve(url,"file-" + re.sub("/","",url))
 
 # TODO
 # get most recent content
